92b668fc50a93daee2da9f7d0c79ec2c41809556
Tim Reilly <timreilly@live.com>
2017-10-04 13:50:25 -0700

    now with a static ip

--- models/images.js
+++ models/images.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 var request = require('request');
 
-const URL = 'http://104.214.27.240:3000/images';  
+const URL = 'http://13.85.67.156:3000/images';  
 
 // const URL = 'http://localhost:8000/images' 
 /** 
