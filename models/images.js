09a22359b80dac92abee29119d4eba680dc4871f
Tim Reilly <timreilly@live.com>
2017-10-04 11:00:50 -0700

    using the outbound ip address of the pub sub server

--- models/images.js
+++ models/images.js
@@ -5,6 +5,7 @@ var request = require('request');
 
 const URL = 'https://calhack-pubsub-test.azurewebsites.net/images';  
 
+// const URL = 'http://localhost:8000/images' 
 /** 
  * Get badges from the pub/sub server
  * @param {Function} callbacks
