7aa98d2defcaa6d320fabe61e5e74a15e40341d9
Tim Reilly <timreilly@live.com>
2017-10-03 17:34:32 -0700

    format

--- models/images.js
+++ models/images.js
@@ -1,13 +1,13 @@
-'use strict'; 
+'use strict';
 
-var request = require('request'); 
+var request = require('request');
 /**
  * Get badges from the pub/sub server
  * @param {Function} callback
  */
 
- exports.get = function(callback){
-     request('https://calhack-pubsub-test.azurewebsites.net/images', function(err, response, body){
-         callback(err, JSON.parse(body));
-     });
- }
+exports.get = function (callback) {
+    request('https://calhack-pubsub-test.azurewebsites.net/images', function (err, response, body) {
+        callback(err, JSON.parse(body));
+    });
+}
