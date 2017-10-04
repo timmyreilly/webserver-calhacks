c1cd68681be928730324f7ada9c01e516426eeb6
Tim Reilly <timreilly@live.com>
2017-10-04 16:33:33 -0700

    removed some junk

--- public/js/index.js
+++ public/js/index.js
@@ -21,23 +21,7 @@ $(function () {
 
         $('.gallery').prepend(galleryItem); 
 
-        // var post = $([
-        //     '<div class="post-container">',
-        //     '   <img class="image-wrapper" src="' + image.imageURL + '">',
-        //     '   <h2>From: ' + phoneNumber + '</h2>',            
-        //     '   <p>' + caption + '</p>',
-        //     '</div>'
-
-        // ].join("\n"));
-
-        // $('.posts').prepend(post);
-
-        // setTimeout(function () {
-        //     post.addClass('on');
-        // }, 0);
-
         setTimeout(function(){
-            $(".image-wrapper").addClass('on'); 
             $(".gallery__image").addClass('on'); 
         }, 0); 
     });
